68aa6845a9ca231e3461567005968106cff684ce
Chibuike Maduabuchi <chibuikemaduabuchi2023@gmail.com>
2025-07-02 16:58:32 +0100

    added utils.js changes

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,5 @@
-export function visualStroke(input, value, error, isValid) {
+export function visualStroke(input, value, error) {
 	if (error) return "border-status-error";
 	if (input === value) return "border-2 border-primary";
-	if (isValid) return " border-status-success";
 	return "border-stroke";
 }
